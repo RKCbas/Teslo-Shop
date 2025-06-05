efc7a9e964e83c3bc7cafc5b1f09a30646af4b33
RKCbas <sebashp2015@hotmail.com>
2025-06-05 17:52:46 -0600

    # Class 4 Tarjetas de productos y FadeIn animation ✨Feat: Added a new component that display info for a product and sends you to that product with a fadeIn animation of 0.3 s from opacity 0 to opacity 1 and configure it in the global styles to get it from the intellisense of tailwind

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
