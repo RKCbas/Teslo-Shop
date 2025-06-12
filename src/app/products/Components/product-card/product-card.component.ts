2211db305f176dd65a9e09dfc0ada5f2257e37ee
RKCbas <sebashp2015@hotmail.com>
2025-06-12 17:15:32 -0600

    # Class 8 images pipe ✨Feat: Created a pipe to return the image url from the file name concatenating it to a endpoint and validate it in case if its given a string or a string[] and in case that there is no image display a placeholder

--- src/app/products/Components/product-card/product-card.component.ts
+++ src/app/products/Components/product-card/product-card.component.ts
@@ -1,25 +1,27 @@
 import { NgClass, SlicePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, /*computed*/, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'product-card',
   imports: [
     RouterLink,
     SlicePipe,
-    NgClass
+    NgClass,
+    ProductImagePipe
   ],
   templateUrl: './product-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
   product = input.required<Product>();
-  imageUrl = computed(() => {
-    if (!this.product().images?.[0]?.length || this.product().images?.[0] === 'undefined') {
-      // Placeholder image URL, you can replace it with a more suitable placeholder
-      return 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
-    }
-    return `http://localhost:3000/api/files/product/${this.product().images[0]}`
-  })
+  // imageUrl = computed(() => {
+  //   if (!this.product().images?.[0]?.length || this.product().images?.[0] === 'undefined') {
+  //     // Placeholder image URL, you can replace it with a more suitable placeholder
+  //     return 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
+  //   }
+  //   return `http://localhost:3000/api/files/product/${this.product().images[0]}`
+  // })
 }
