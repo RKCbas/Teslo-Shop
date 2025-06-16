1e7b7c613007ca11217816a69886311df7043a7a
RKCbas <sebashp2015@hotmail.com>
2025-06-16 16:17:46 -0600

    # Class 13 pagination ✨Feat: Give the url the current page as a param

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -10,6 +11,8 @@ export class PaginationComponent {
 
   totalPages = input<number>(0);
   currentPage = input<number>(1);
+
+  activePage = linkedSignal(this.currentPage);
 
   getPagesList = computed(() => {
     return Array.from({ length: this.totalPages() }, (_, i) => i + 1)
