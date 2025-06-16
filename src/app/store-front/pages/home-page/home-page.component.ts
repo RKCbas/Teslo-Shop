99fce3127813c4867cd1036f3e9934421a5d2446
RKCbas <sebashp2015@hotmail.com>
2025-06-16 16:42:54 -0600

    # Class 14 pagination with rxResource ✨Feat: Implemented a offset depending of the page

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
-import { rxResource } from '@angular/core/rxjs-interop'
+import { rxResource, toSignal } from '@angular/core/rxjs-interop'
 
 import { ProductsService } from '@products/services/products.service';
 import { ProductsListComponent } from '@products/Components/products-list/products-list.component';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 
 
@@ -13,7 +15,7 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
   imports: [
     ProductsListComponent,
     PaginationComponent
-],
+  ],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -21,10 +23,22 @@ export class HomePageComponent {
 
   productsService = inject(ProductsService);
 
+  activatedRoute = inject(ActivatedRoute)
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map(params => (params.get('page') ? +params.get('page')! : 1)),
+      map(page => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1,
+    }
+  )
+
   productsResource = rxResource({
-    request: () => ({}),
-    loader: ({request}) => {
-      return this.productsService.getProducts({});
+    request: () => ({ page: this.currentPage() - 1 }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({ offset: request.page * 9 });
     }
   })
 
