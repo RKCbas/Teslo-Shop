6841ff00624ae75d1a314328e1da3c719700a5a7
RKCbas <sebashp2015@hotmail.com>
2025-06-16 09:55:03 -0600

    # Class 10 product carousel ✨Feat: installed [swiper](https://swiperjs.com) to create a carousel as a component

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
+import { ProductCarouselComponent } from "../../../products/Components/product-carousel/product-carousel.component";
 
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
