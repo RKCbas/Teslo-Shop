5785dceae5d9eb1fc5a4db233ea7e543a206916d
RKCbas <sebashp2015@hotmail.com>
2025-06-23 11:03:57 -0600

    # Class 19 Auth Service ✨Feat: Basic configuration of the Auth Service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { User } from '@auth/interfaces/user.interface';
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+@Injectable({ providedIn: 'root' })
+export class ServiceNameService {
+
+  private _authStatus = signal<AuthStatus>('checking');
+  private _user = signal<User | null>(null);
+  private _token = signal<string | null>(null);
+
+  private readonly http = inject(HttpClient);
+
+  authStatus = computed<AuthStatus>(() => {
+    if (this._authStatus() === 'checking') return 'checking';
+
+    if (this._user()) return 'authenticated';
+
+    return 'not-authenticated'
+
+  });
+
+  user = computed(() => this._user());
+  token = computed(() => this._token());
+
+}
