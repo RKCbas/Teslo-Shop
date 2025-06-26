66b0dcc4670cf939a10223cbf26893defab20ba9
RKCbas <sebashp2015@hotmail.com>
2025-06-26 16:17:47 -0600

    # Class 24 User modification to the navbar ✨Feat: Added to the navbar a text that shows the user fullname in case that the user is logged in and also added a button to logout and validated the authStatus

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { User } from '@auth/interfaces/user.interface';
